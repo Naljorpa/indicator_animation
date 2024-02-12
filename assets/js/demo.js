6de304d68a60bcbb0cb440b9cd658a1d769386cb
nifon11 <nifon11@ulaval.ca>
2024-02-12 16:40:05 -0500

    added an intersection observer

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  function animatePartenariat() {
+  function animatePartenariat1() {
     let currentPercentage = 0;
     let percentage = 52;
     let increment = 1;
@@ -53,7 +53,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
       if (currentNumber >= number) {
         clearInterval(intervalId);
-        document.getElementById("formation-number").textContent = number.toLocaleString();
+        document.getElementById("formation-number").textContent =
+          number.toLocaleString();
       }
     }, interval);
 
@@ -74,7 +75,89 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  animatePartenariat();
-  animateInnovation();
-  animateFormation();
+  function animatePartenariat2() {
+    let currentPercentage = 0;
+    let percentage = 60;
+    let increment = 1;
+    let interval = 40;
+
+    let intervalId = setInterval(function () {
+      document.getElementById("percentage2").textContent =
+        currentPercentage + "%";
+      currentPercentage += increment;
+
+      if (currentPercentage >= percentage) {
+        clearInterval(intervalId);
+        document.getElementById("percentage2").textContent = percentage + "%";
+      }
+    }, interval);
+
+    let number = 190 * (1 - percentage / 100);
+
+    console.log(number);
+    document.documentElement.style.setProperty("--end-value", number);
+  }
+
+  const observer = new IntersectionObserver(callback, {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.9,
+  });
+
+  const animateBoxes = document.querySelectorAll(".animate-box");
+
+  animateBoxes.forEach((animateBox) => {
+    observer.observe(animateBox);
+  });
+
+  let entryTrigger = 0;
+  function callback(entries, observer) {
+
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+
+         entryTrigger++;
+
+         console.log(entryTrigger);
+
+        if (entry.target.classList.contains("partenariat1")) {
+          const circlePie = entry.target.querySelector(".x1");
+          circlePie.style.animation = "dash-animation 2s ease-in forwards";
+          animatePartenariat1();
+
+
+        }
+
+        if (entry.target.classList.contains("innovation")) {
+          const arrow = entry.target.querySelector(".fleche");
+          arrow.style.animation = "deplacementFleche 2s forwards";
+          animateInnovation();
+
+        }
+
+        if (entry.target.classList.contains("partenariat2")) {
+          const circlePie = entry.target.querySelector(".x2");
+          circlePie.style.animation = "dash-animation 2s ease-in forwards";
+          animatePartenariat2();
+
+        }
+        if (entry.target.classList.contains("formation")) {
+          const bars = entry.target.querySelectorAll(".bar");
+          bars.forEach((bar) => {
+            bar.style.animation = "reveal 1s ease forwards";
+          });
+          animateFormation();
+        }
+
+      }
+    });
+    if (entryTrigger == 4) {
+      observer.disconnect();
+    }
+  }
+
+  // animatePartenariat();
+  // animateInnovation();
+  // animateFormation();
+  // animatePartenariat2();
 });
